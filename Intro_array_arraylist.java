99e0b2a9ee02f133aa847cf25d05d5a46e79ed2b
nizamudd.in <mdnizammoiz@gmail.com>
2026-06-08 15:41:08 +0530

    Add array traversal examples

---
+++ Intro_array_arraylist.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Intro_array_arraylist {
+ public static void main(String args[]){
+    // array example
+    int[] arr ={1,2,3,4};
+    System.out.println(Arrays.toString(arr));
+    
+    // arraylist example
+    ArrayList<Integer> list=new ArrayList<>();
+    list.add(12);
+    list.add(13);
+    System.out.println(list);
+}}
